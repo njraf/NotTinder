ea3007335c6ac0080cb5538846e073d8e3bbff3a
Nick Rafala <nrafala@outlook.com>
2025-08-01 19:34:07 -0400

    Created name cards to accept or reject a person

--- app/src/main/java/com/example/nottinder/MatchMakingScreen.kt
+++ app/src/main/java/com/example/nottinder/MatchMakingScreen.kt
@@ -1,20 +1,56 @@
 package com.example.nottinder
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
+@Preview(showBackground = true)
 @Composable
 fun MatchMakingScreen() {
 
     val viewModel: MatchMakingViewModel = viewModel()
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
-    LazyColumn {
-        items(viewModel.candidates) { candidate ->
-            Text(candidate.name)
+    ProfileCard(state.currentCandidate.name, { viewModel.nextCandidate() }, { viewModel.nextCandidate() })
+}
+
+@Composable
+fun ProfileCard(name: String, onYes: () -> Unit, onNo: () -> Unit) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Column {
+            Text(
+                text = name,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+
+            Row {
+                Button(onClick = onYes) {
+                    Text("Yes")
+                }
+                Button(onClick = onNo) {
+                    Text("No")
+                }
+            }
         }
     }
-
 }
--- app/src/main/java/com/example/nottinder/MatchMakingViewModel.kt
+++ app/src/main/java/com/example/nottinder/MatchMakingViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.nottinder
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class MatchMakingState(
+    val currentCandidate: User = User("No Users Found")
+)
 
 class MatchMakingViewModel : ViewModel() {
-    val candidates = mutableStateListOf<User>(
+    private var candidates = mutableListOf<User>(
         User("Jack"),
         User("Alan"),
         User("Jessica"),
@@ -12,7 +19,29 @@ class MatchMakingViewModel : ViewModel() {
         User("Sammy"),
     )
 
+    private var _state: MutableStateFlow<MatchMakingState> = MutableStateFlow(MatchMakingState())
+    var state: StateFlow<MatchMakingState> = _state.asStateFlow()
+
+
+    init {
+        _state.update { currentState ->
+            currentState.copy(
+                currentCandidate = candidates.first()
+            )
+        }
+    }
+
     fun nextCandidate() {
-        candidates.drop(0)
+        candidates = candidates.drop(1).toMutableList()
+
+        if(candidates.isEmpty()) {
+            return
+        }
+
+        _state.update { currentState ->
+            currentState.copy(
+                currentCandidate = candidates.first()
+            )
+        }
     }
 }
